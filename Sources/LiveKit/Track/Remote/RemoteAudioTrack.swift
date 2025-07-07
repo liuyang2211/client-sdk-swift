b71ee9eb9cd7e2e78fbf5f300b4b8aafb560d70b
刘兴邦 <liuxingbang@vjsp.cn>
2025-07-07 17:42:20 +0800

    提交测试

--- Sources/LiveKit/Track/Remote/RemoteAudioTrack.swift
+++ Sources/LiveKit/Track/Remote/RemoteAudioTrack.swift
@@ -115,6 +115,7 @@ public class RemoteAudioTrack: Track, RemoteTrack, AudioTrack {
 }
 
 extension RemoteAudioTrack: AudioRenderer {
+    //
     public func render(sampleBuffer: CMSampleBuffer) {
         _rendererState.audioRenderers.notify { audioRenderer in
             audioRenderer.render?(sampleBuffer: sampleBuffer)
