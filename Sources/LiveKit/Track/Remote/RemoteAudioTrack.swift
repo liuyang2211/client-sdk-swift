fad76d961b1635864b45f95b1c166868440b2515
CJXB <45448945+CJXB@users.noreply.github.com>
2025-07-08 17:21:27 +0800

    Update RemoteAudioTrack.swift

--- Sources/LiveKit/Track/Remote/RemoteAudioTrack.swift
+++ Sources/LiveKit/Track/Remote/RemoteAudioTrack.swift
@@ -281,7 +281,7 @@ extension RemoteAudioTrack: AudioRenderer {
         
         // 创建 AVAudioFormat 对象（修复可选值问题）
         let audioFormat = AVAudioFormat(
-            commonFormat: .pcmFormatInt32,
+            commonFormat: .pcmFormatFloat32,
             sampleRate: Double(streamDesc.pointee.mSampleRate),
             channels: AVAudioChannelCount(streamDesc.pointee.mChannelsPerFrame),
             interleaved: streamDesc.pointee.mFormatFlags & kAudioFormatFlagIsNonInterleaved == 0
