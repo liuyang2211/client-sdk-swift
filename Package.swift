3e4de099d9d001ecaaba55a4e54df2653c2fc47a
Lewis <lewis-wsq@163.com>
2026-03-03 16:58:21 +0800

    更换依赖到gitee

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     ],
     dependencies: [
         // LK-Prefixed Dynamic WebRTC XCFramework
-        .package(url: "https://github.com/livekit/webrtc-xcframework.git", exact: "125.6422.07"),
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.26.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.4"),
+        .package(url: "https://gitee.com/lewiswsq2211/webrtc-xcframework.git", exact: "125.6422.07"),
+        .package(url: "https://gitee.com/lewiswsq2211/swift-protobuf.git", from: "1.26.0"),
+        .package(url: "https://gitee.com/lewiswsq2211/swift-log.git", from: "1.5.4"),
         // Only used for DocC generation
-        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0"),
+        .package(url: "https://gitee.com/lewiswsq2211/swift-docc-plugin.git", from: "1.3.0"),
         // Only used for Testing
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.13.4"),
+        .package(url: "https://gitee.com/lewiswsq2211/jwt-kit.git", from: "4.13.4"),
     ],
     targets: [
         .target(
